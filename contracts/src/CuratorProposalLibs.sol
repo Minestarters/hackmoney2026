116bc9d2e5c973f4f0eec34d1ef5793bb26dc712
Reuben Rodrigues <reubenrodrigues24@gmail.com>
2026-02-02 19:24:56 +0530

    feat(contracts): initial curator proposal

---
+++ contracts/src/CuratorProposalLibs.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+library ECDSA {
+    function recover(bytes32 hash, bytes memory signature) internal pure returns (address) {
+        if (signature.length != 65) {
+            return address(0);
+        }
+
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            r := mload(add(signature, 0x20))
+            s := mload(add(signature, 0x40))
+            v := byte(0, mload(add(signature, 0x60)))
+        }
+
+        if (v < 27) {
+            v += 27;
+        }
+        if (v != 27 && v != 28) {
+            return address(0);
+        }
+
+        return ecrecover(hash, v, r, s);
+    }
+}
--- contracts/src/MineStarters.sol
+++ contracts/src/MineStarters.sol
@@ -2,10 +2,11 @@
 pragma solidity ^0.8.24;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 /// @title MineStarters
-contract MineStarters is ERC20 {
-    constructor() ERC20("Mine Starters", "MINE") {}
+contract MineStarters is ERC20, ERC20Permit {
+    constructor() ERC20("Minestarters", "MINE") ERC20Permit("Minestarters") {}
 
     function mint(address to, uint256 amount) external {
         _mint(to, amount);
